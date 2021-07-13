239b9a2e95aa2c9a56411606e0b903ab814e48b2
warNebula <colinnebula@hotmail.com>
2021-07-13 14:55:36 -0400

    updated the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,6 +85,8 @@ $("#task-form-modal .btn-primary").click(function() {
 });
 
 
+
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -104,6 +106,37 @@ $(".list-group").on("click", "p", function() {
   textInput.trigger("focus")
   .addClass("form-control")
   .val(text);
+});
+//Svave and change the date back from edit mode
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
 });
 
 $(".list-group").on("blur", "textarea", function() {
@@ -134,7 +167,26 @@ var index = $(this)
 tasks[status][index].text = text;
 saveTasks();
 
+});
 
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
 });
 
 // load tasks for the first time

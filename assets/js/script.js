09af16eb94df42d4a1e943a5de8a576a23e33e58
warNebula <colinnebula@hotmail.com>
2021-07-22 10:40:25 -0400

    updated the index.html and script files

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -44,75 +45,7 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-// enable draggable/sortable feature on list-group elements
-$(".card .list-group").sortable({
-  // enable dragging across lists
-  connectWith: $(".card .list-group"),
-  scroll: false,
-  tolerance: "pointer",
-  helper: "clone",
-  activate: function(event, ui) {
-    console.log(ui);
-  },
-  deactivate: function(event, ui) {
-    console.log(ui);
-  },
-  over: function(event) {
-    console.log(event);
-  },
-  out: function(event) {
-    console.log(event);
-  },
-  update: function() {
-    var tempArr = [];
 
-    // loop over current set of children in sortable list
-    $(this)
-      .children()
-      .each(function() {
-        // save values in temp array
-        tempArr.push({
-          text: $(this)
-            .find("p")
-            .text()
-            .trim(),
-          date: $(this)
-            .find("span")
-            .text()
-            .trim()
-        });
-      });
-
-    // trim down list's ID to match object property
-    var arrName = $(this)
-      .attr("id")
-      .replace("list-", "");
-
-    // update array on tasks object and save
-    tasks[arrName] = tempArr;
-    saveTasks();
-  },
-  stop: function(event) {
-    $(this).removeClass("dropover");
-  }
-});
-
-// trash icon can be dropped onto
-$("#trash").droppable({
-  accept: ".card .list-group-item",
-  tolerance: "touch",
-  drop: function(event, ui) {
-    // remove dragged element from the dom
-    ui.draggable.remove();
-
-  },
-  over: function(event, ui) {
-    console.log(ui);
-  },
-  out: function(event, ui) {
-    console.log(ui);
-  }
-});
 
 
 // modal was triggered
@@ -210,7 +143,7 @@ $(".list-group").on("click", "span", function() {
 });
 
 // value of due date was changed
-$(".list-group").on("change", "input[type='text']", function() {
+$(".list-group").on("blur", "input[type='text']", function() {
   var date = $(this).val();
 
   // get status type and position in the list
@@ -239,9 +172,10 @@ $("#remove-tasks").on("click", function() {
     tasks[key].length = 0;
     $("#list-" + key).empty();
   }
-  console.log(tasks);
   saveTasks();
 });
 
 // load tasks for the first time
 loadTasks();
+
+
